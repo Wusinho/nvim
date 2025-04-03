2849e631a8bfdd1dc55aecf7781a36de6418764f
Heber Lazo <heberlazo@gmail.com>
2025-04-02 23:20:35 -0500

    removed vim-options

--- init.lua
+++ init.lua
@@ -11,6 +11,27 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
-require("vim-options")
+
+--require("vim-options")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
+vim.opt.swapfile = false
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Left>",  ":vertical resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Up>",    ":resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Down>",  ":resize -2<CR>", { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
+
 require("lazy").setup("plugins")
 

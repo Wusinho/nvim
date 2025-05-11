49737f661e6d4d0f85c3fca7c2d2c06668d1eb69
Heber Lazo <heberlazo@gmail.com>
2025-05-11 18:51:29 -0500

    correct gd for class def

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
     -- Shared on_attach function
     local function on_attach(client, bufnr)
       local bufopts = { noremap = true, silent = true, buffer = bufnr }
-      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, bufopts)
+      vim.keymap.set('n', '<leader>cd', vim.lsp.buf.definition, bufopts)
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
       vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)

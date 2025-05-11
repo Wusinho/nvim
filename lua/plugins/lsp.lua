451d94f6a42465ecc8fac8017f4de8faea1ba1cd
Heber Lazo <heberlazo@gmail.com>
2025-05-11 18:26:07 -0500

    added rails and lua config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,16 +2,87 @@ return {
   "neovim/nvim-lspconfig",
   config = function()
     local lspconfig = require("lspconfig")
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
 
+    -- Shared on_attach function
+    local function on_attach(client, bufnr)
+      local bufopts = { noremap = true, silent = true, buffer = bufnr }
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, bufopts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end,
+        })
+      end
+    end
+
+    -- TypeScript LSP
     lspconfig.ts_ls.setup({
-      on_attach = function(client, bufnr)
-        local bufopts = { noremap=true, silent=true, buffer=bufnr }
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
 
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    -- Ruby LSP with Rails Add-on and Standard formatting/linting
+
+    lspconfig.ruby_lsp.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+      cmd = { "/home/hlazo/.rbenv/shims/ruby-lsp" },
+      filetypes = { "ruby", "eruby" },
+      settings = {
+        rubyLsp = {
+          formatter = 'standard',
+          linters = { 'standard' },
+          addonSettings = {
+            ["Ruby LSP Rails"] = {
+              enablePendingMigrationsPrompt = true,
+            },
+          },
+        },
+      },
+    })
+
+    -- Lua LSP
+    lspconfig.lua_ls.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          runtime = {
+            -- Use LuaJIT for Neovim
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            -- Recognize the global 'vim'
+            globals = { 'vim' },
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false, -- Avoid "Do you want to configure lua-dev?"
+          },
+          telemetry = {
+            enable = false, -- No telemetry data collection
+          },
+        },
+      },
+    })
+    -- ESLint LSP
+    lspconfig.eslint.setup({
+      on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          command = "EslintFixAll",
+        })
       end,
+      capabilities = capabilities,
     })
   end
 }
--- lua/plugins/slint.lua
+++ lua/plugins/slint.lua
@@ -1,13 +1,21 @@
 return {
   "nvimtools/none-ls.nvim",
   config = function()
-    local null_ls = require("null-ls") -- 🔥 keep this as "null-ls", NOT "none-ls"
+    local null_ls = require("null-ls") -- keep as null-ls
+
+    local sources = {
+      null_ls.builtins.formatting.prettier,
+    }
+
+    -- Conditionally enable ESLint if config file exists
+    if vim.fn.filereadable(".eslintrc.js") == 1 or vim.fn.filereadable(".eslintrc.json") == 1 then
+      table.insert(sources, null_ls.builtins.diagnostics.eslint)
+      table.insert(sources, null_ls.builtins.code_actions.eslint) -- Add code actions (fixes)
+    end
 
     null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.prettier,
-      },
+      sources = sources,
+      root_dir = require("null-ls.utils").root_pattern(".git", "package.json", ".eslintrc.js", ".eslintrc.json"),
     })
   end
 }
-

fbbe3fa8105e91b937518338bc094fb12a268d51
Heber Lazo <heberlazo@gmail.com>
2025-05-31 08:39:28 -0500

    corrected auto save

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,11 +1,11 @@
- return {
+return {
   {
     "pocco81/auto-save.nvim",
     config = function()
       require("auto-save").setup({
-        enabled = true,   -- Enable auto-save
+        enabled = true,                            -- Enable auto-save
         execution_message = {
-          message = function() return "" end, -- Hide message on save
+          message = function() return "" end,      -- Hide message on save
         },
         events = { "InsertLeave", "TextChanged" }, -- Auto-save on leaving insert mode & changes
         conditions = {
@@ -13,10 +13,9 @@
           filename_is_not = {},
           filetype_is_not = { "gitcommit" }, -- Avoid auto-saving commits
         },
-        write_all_buffers = false, -- Save only the current buffer
-        debounce_delay = 200, -- Save delay in milliseconds
+        write_all_buffers = false,           -- Save only the current buffer
+        debounce_delay = 200,                -- Save delay in milliseconds
       })
     end,
   },
 }
- 

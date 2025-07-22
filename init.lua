61ba09e58e5492025a799b9e7d22f2683f93356d
Heber Lazo <heberlazo@gmail.com>
2025-07-22 15:13:27 -0500

    added small config

--- init.lua
+++ init.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.g.mapleader = " "
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,10 +5,10 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "lua", "javascript", "typescript", "css", "bash", "regex", "sql", "markdown", "markdown_inline", "json", "html", "yaml"},
+        ensure_installed = { "lua", "javascript", "typescript", "css", "bash", "regex", "sql", "markdown", "markdown_inline", "json", "html", "yaml", "ruby" },
         auto_install = true,
-        highlight = {enable = true },
-        indent ={enable = true}
+        highlight = { enable = true },
+        indent = { enable = true }
       })
     end
   }

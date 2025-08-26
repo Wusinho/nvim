d7dded7dd0fcc6f30e11d7cfd53b206464399489
Wusinho <heberlazo@gmail.com>
2025-08-26 09:30:26 -0500

    added config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,14 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
     build = ":TSUpdate",
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
         ensure_installed = { "lua", "javascript", "typescript", "css", "bash", "regex", "sql", "markdown", "markdown_inline", "json", "html", "yaml", "ruby" },
+       sync_install = false,
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true }

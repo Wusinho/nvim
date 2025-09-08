33c33c33c1e2f6dbc34b1882325bcb09408780f5
Wusinho <heberlazo@gmail.com>
2025-09-08 13:44:10 -0500

    added referneces

--- lua/lsp/shared.lua
+++ lua/lsp/shared.lua
@@ -5,6 +5,7 @@ M.capabilities = vim.lsp.protocol.make_client_capabilities()
 function M.on_attach_common(client, bufnr)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', '<leader>cd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', '<leader>cr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
